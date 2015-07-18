f9f26e644b88950f9f5afc72ed7cd829dbfe2b65
Carlos Llongo <carlos.llongo@gmail.com>
2015-07-18 18:45:21 +0200

    Added the basic functionality for the kanji review flow (draw, check and select right or wrong).

--- src/js/kanjireview/drawing/DrawingController.js
+++ src/js/kanjireview/drawing/DrawingController.js
@@ -5,24 +5,72 @@
         .module('kanjireview.drawing')
         .controller('DrawingController', DrawingController);
 
-    DrawingController.$inject = ['$scope'];
+    DrawingController.$inject = ['ReviewService'];
 
-    function DrawingController($scope){
+    function DrawingController(ReviewService){
 
         var vm = this;
 
-        vm.bDrawingBoardExists = false;
-        vm.oDrawingBoard = null;
         vm.oVersion;
+        vm.sState = 'writing';
 
+        vm.checkKanji = checkKanji;
         vm.clearDrawingBoard = clearDrawingBoard;
+        vm.getCurrentKanji = getCurrentKanji;
+        vm.getCurrentKanjiId = getCurrentKanjiId;
+        vm.getCurrentKanjiKeyword = getCurrentKanjiKeyword;
+        vm.getReviewedKanji = getReviewedKanji;
+        vm.isCheckingState = isCheckingState;
+        vm.successfulWriting = successfulWriting;
+        vm.wrongWriting = wrongWriting;
+
+        ReviewService.init();
 
         // PUBLIC //////////////////////////////////////////////////////////////
+
+        function checkKanji(){
+            vm.sState = 'checking';
+        }
 
         function clearDrawingBoard(){
             vm.oVersion = 0;
         }
 
+        function getCurrentKanji(){
+            return ReviewService.getCurrentKanji();
+        }
+
+        function getCurrentKanjiId(){
+            return ReviewService.getCurrentKanjiId();
+        }
+
+        function getCurrentKanjiKeyword(){
+            return ReviewService.getCurrentKanjiKeyword();
+        }
+
+        function getReviewedKanji(){
+            return ReviewService.getReviewedKanji();
+        }
+
+        function isCheckingState(){
+            return vm.sState === 'checking';
+        }
+
+        function successfulWriting(){
+            ReviewService.successfulWriting();
+
+            clearDrawingBoard();
+
+            vm.sState = 'writing';
+        }
+
+        function wrongWriting(){
+            ReviewService.wrongWriting();
+
+            clearDrawingBoard();
+
+            vm.sState = 'writing';
+        }
 
         // PRIVATE /////////////////////////////////////////////////////////////
     }
