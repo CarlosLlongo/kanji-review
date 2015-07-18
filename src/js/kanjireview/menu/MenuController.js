8ed3701fb4e224e8af8d88499e2904951d02284c
Carlos Llongo <carlos.llongo@gmail.com>
2015-07-18 18:47:35 +0200

    New controller for the menu.

---
+++ src/js/kanjireview/menu/MenuController.js
@@ -0,0 +1,25 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('kanjireview.menu')
+        .controller('MenuController', MenuController);
+
+    MenuController.$inject = ['ReviewService'];
+
+    function MenuController(ReviewService){
+
+        var vm = this;
+
+        vm.endReview = endReview;
+
+        // PUBLIC //////////////////////////////////////////////////////////////
+
+        function endReview(){
+            console.log('yay!');
+        }
+
+        // PRIVATE /////////////////////////////////////////////////////////////
+    }
+
+})();
