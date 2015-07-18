c2079fa7c19ef170696ac80da2422fdfc501b4e8
Carlos Llongo <carlos.llongo@gmail.com>
2015-07-18 18:47:06 +0200

    Added the menu, review and start modules.

--- src/js/KanjiReview.js
+++ src/js/KanjiReview.js
@@ -4,12 +4,15 @@
     angular
         .module('kanjireview', [
             'kanjireview.drawing',
-            'kanjireview.jander',
+            'kanjireview.menu',
+            'kanjireview.review',
+            'kanjireview.start',
             'ionic',
             'pw.canvas-painter'
         ]);
 
     angular.module('kanjireview.drawing', []);
-    angular.module('kanjireview.jander', []);
-
+    angular.module('kanjireview.menu', []);
+    angular.module('kanjireview.review', []);
+    angular.module('kanjireview.start', []);
 })();
