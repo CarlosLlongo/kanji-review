07260a3557c731f83bcab63890e67086743c9231
Carlos Llongo <carlos.llongo@gmail.com>
2015-07-28 21:26:02 +0200

    Added the getLearnedKanji and getNextKanji methods.

--- src/js/kanjireview/review/ReviewEngine.js
+++ src/js/kanjireview/review/ReviewEngine.js
@@ -1,6 +1,20 @@
 'use strict';
 
-function ReviewEngine(){
+function ReviewEngine(nLearnedKanji, oKanjiCollection){
 
     var oReviewEngine = this;
+
+    oReviewEngine.nLearnedKanji = nLearnedKanji;
+    oReviewEngine.oKanjiCollection = oKanjiCollection;
+
+    oReviewEngine.getLearnedKanji = getLearnedKanji;
+    oReviewEngine.getNextKanji = getNextKanji;
+
+    function getLearnedKanji(){
+        return oReviewEngine.nLearnedKanji;
+    }
+
+    function getNextKanji(){
+        return new Kanji('3', 'N', 'water');
+    }
 }
