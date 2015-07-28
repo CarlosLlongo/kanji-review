19490594141057479c3ade3940a6eaec4f8e40a6
Carlos Llongo <carlos.llongo@gmail.com>
2015-07-28 21:27:18 +0200

    Added test for the getNextKanji method.

--- src/spec/kanjireview/review/ReviewEngineTests.js
+++ src/spec/kanjireview/review/ReviewEngineTests.js
@@ -4,6 +4,36 @@ describe('ReviewEngine', function(){
         expect(new ReviewEngine()).toBeDefined();
     });
 
+    it("can set a learned kanji number", function () {
+        var oKanjiCollection = new KanjiCollection();
+
+        var oReviewEngine = new ReviewEngine(51, oKanjiCollection);
+
+        expect(oReviewEngine.getLearnedKanji()).toEqual(51);
+    });
+
+    it("can get next kanji", function () {
+        var oKanjiCollection = new KanjiCollection();
+
+        oKanjiCollection.putKanji(new Kanji('1', '雨', 'rain'));
+        oKanjiCollection.putKanji(new Kanji('2', '雷', 'thunder'));
+        oKanjiCollection.putKanji(new Kanji('3', '雪', 'snow'));
+        oKanjiCollection.putKanji(new Kanji('4', '水', 'water'));
+
+        var oReviewEngine = new ReviewEngine(oKanjiCollection);
+
+        var oNextKanji = oReviewEngine.getNextKanji();
+
+        expect(typeof oNextKanji).toEqual('object');
+        expect(typeof oNextKanji.getId()).toEqual('string');
+        expect(typeof oNextKanji.getCharacter()).toEqual('string');
+        expect(typeof oNextKanji.getKeyword()).toEqual('string');
+
+        var oAnotherKanji = oReviewEngine.getNextKanji();
+
+        //expect(oAnotherKanji).not.toEqual(oNextKanji);
+    });
+
 
 
 });
