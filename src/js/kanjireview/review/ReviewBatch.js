e65804359e3be248f5317ec1aee93bbdf6c2b32e
Carlos Llongo <carlos.llongo@gmail.com>
2015-10-13 14:40:17 +0200

    Finished the populate method. Added documentation.

--- src/js/kanjireview/review/ReviewBatch.js
+++ src/js/kanjireview/review/ReviewBatch.js
@@ -1,11 +1,39 @@
+/**
+ * This class will generate a batch of kanji IDs for a review session. The review batch will contain as many difficult
+ * Kanjis as available, and the rest will be divided equally between medium and easy kanjis.
+ * @param nLearnedKanji The total number of learned kanjis.
+ * @param nNewLearnedKanji The number of kanji learned since the last review.
+ * @param oOptions Contains the dependencies with other objects
+ * @constructor
+ */
 function ReviewBatch(nLearnedKanji, nNewLearnedKanji, oOptions){
 
     var oReviewBatch = this;
+    /**
+     * The maximum size of the review batch.
+     * @type {Integer}
+     */
+    var nMaxReviewBatchSize = 100;
 
+    /**
+     * The total number of learned kanjis.
+     * @type {Integer}
+     */
     oReviewBatch.nLearnedKanji = nLearnedKanji;
+    /**
+     * The number of kanji learned since the last review.
+     * @type {Integer}
+     */
     oReviewBatch.nNewLearnedKanji = nNewLearnedKanji;
+    /**
+     * The array containing the kanji IDs for this batch.
+     * @type {Array}
+     */
     oReviewBatch.aStorage = [];
-    oReviewBatch.nMaxReviewBatchSize = 100;
+    /**
+     * Object from which to obtain the kanji of different difficulties.
+     * @type {KanjiDifficultyManager}
+     */
     oReviewBatch.oKanjiDifficultyManager = null;
 
     oReviewBatch.populate = populate;
@@ -15,55 +43,68 @@ function ReviewBatch(nLearnedKanji, nNewLearnedKanji, oOptions){
 
     initOptions(oOptions);
 
+    /**
+     * Returns the size of the batch.
+     * @returns {Array.length|*} The size of the batch.
+     */
     function getSize(){
         return oReviewBatch.aStorage.length;
     }
 
+    /**
+     * Returns the batch.
+     * @returns {Array} The array with the kanji IDs that compose this batch.
+     */
     function getBatch(){
         return oReviewBatch.aStorage;
     }
 
+    /**
+     * Fills the batch by adding the new learned kanji and kanjis of the 3 difficulties, until the maximum size is
+     * reached or there are no more kanjis left.
+     */
     function populate(){
-        if(nNewLearnedKanji <= oReviewBatch.nMaxReviewBatchSize){
+
+        if(nNewLearnedKanji <= nMaxReviewBatchSize){
             for(var i = oReviewBatch.nLearnedKanji; i > nLearnedKanji - nNewLearnedKanji; i--){
-                oReviewBatch.aStorage.push(String(i));
+                oReviewBatch.aStorage.push(i);
             }
         }
         else{
-            var aLearnedIds = fillArray(nLearnedKanji - nNewLearnedKanji + 1, nNewLearnedKanji);
+            var aNewLearnedIds = fillArray(nLearnedKanji - nNewLearnedKanji + 1, nNewLearnedKanji);
 
             var nKanjiId;
-            for(var i = 0; i < oReviewBatch.nMaxReviewBatchSize; i++){
-                nKanjiId = getAndRemoveRandomValue(aLearnedIds);
-                oReviewBatch.aStorage.push(String(nKanjiId));
-            }
-
-            oReviewBatch.oKanjiDifficultyManager.addAllToHard(aLearnedIds);
-        }
-
-        if(oReviewBatch.oKanjiDifficultyManager.getHardStorageSize() > 0){
-            if(oReviewBatch.oKanjiDifficultyManager.getHardStorageSize() > remaining()){
-                var aHardStorage = oReviewBatch.oKanjiDifficultyManager.getHardStorage();
-
-
+            for(var i = 0; i < nMaxReviewBatchSize; i++){
+                nKanjiId = getAndRemoveRandomValue(aNewLearnedIds);
+                oReviewBatch.aStorage.push(nKanjiId);
             }
         }
 
-        if(oReviewBatch.aStorage.length === 100){
-            return;
-        }
+        oReviewBatch.aStorage = oReviewBatch.aStorage.concat(
+            oReviewBatch.oKanjiDifficultyManager.getFromHardCycle(remaining()));
 
-        var nReviewBatchSize = Math.min(oReviewBatch.nLearnedKanji, oReviewBatch.nMaxReviewBatchSize);
+        if(remaining() > 0){
+            oReviewBatch.aStorage = oReviewBatch.aStorage.concat(
+                oReviewBatch.oKanjiDifficultyManager.getFromMediumCycle(Math.ceil(remaining() / 2)));
 
-        for(var i = 1; i <= nReviewBatchSize; i++){
-            oReviewBatch.aStorage.push(String(i));
+            oReviewBatch.aStorage = oReviewBatch.aStorage.concat(
+                oReviewBatch.oKanjiDifficultyManager.getFromEasyCycle(remaining()));
         }
     }
 
+    /**
+     * Returns the available space in the batch until the maximum size will be reached.
+     * @returns {number} The difference between the maximum size and the batch current size.
+     */
     function remaining(){
-        return oReviewBatch.nMaxReviewBatchSize - oReviewBatch.aStorage.length;
+        return nMaxReviewBatchSize - oReviewBatch.aStorage.length;
     }
 
+    /**
+     * Gets the elements in the Option object and assigns them to object properties so then can be used in the class
+     * methods.
+     * @param oOptions The object containing the class dependencies.
+     */
     function initOptions(oOptions){
         if(oOptions instanceof Object){
             if(oOptions.kanjiDifficultyManager){
