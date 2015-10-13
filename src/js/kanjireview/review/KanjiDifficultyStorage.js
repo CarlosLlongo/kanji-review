b25f224860e24e3c008aa519933d649c2742ad70
Carlos Llongo <carlos.llongo@gmail.com>
2015-10-13 15:09:12 +0200

    Fixed the getFromCycle method. Added documentation.

--- src/js/kanjireview/review/KanjiDifficultyStorage.js
+++ src/js/kanjireview/review/KanjiDifficultyStorage.js
@@ -1,8 +1,22 @@
+/**
+ * This class stores the kanji of a certain difficulty and which of those kanji are pending to be reviewed in the
+ * current cycle. When the current cycle is finished, it is refilled with the kanji in the store.
+ * @constructor
+ */
 function KanjiDifficultyStorage(){
 
     var oKanjiDifficultyStorage = this;
 
+    /**
+     * The storage for kanji IDs of a certain difficulty.
+     * @type {Array}
+     */
     oKanjiDifficultyStorage.aStorage = [];
+
+    /**
+     * The kanji IDs pending to be reviewed in the current cycle.
+     * @type {Array}
+     */
     oKanjiDifficultyStorage.aCurrentCycle = [];
 
     oKanjiDifficultyStorage.store = store;
@@ -14,18 +28,29 @@ function KanjiDifficultyStorage(){
     oKanjiDifficultyStorage.setStorage = setStorage;
     oKanjiDifficultyStorage.getStorage = getStorage;
 
+    /**
+     * Adds a kanji ID to the storage.
+     * @param sId The kanji ID
+     */
     function store(sId){
         oKanjiDifficultyStorage.aStorage.push(sId);
     }
 
+    /**
+     * Adds all the kanji IDs in the array to the storage.
+     * @param aIds The array with the kanji IDs to be added.
+     */
     function storeAll(aIds){
-        oKanjiDifficultyStorage.aStorage = oKanjiDifficultyStorage.aStorage.concat(aIds);
+        oKanjiDifficultyStorage.aStorage = removeDuplicates(oKanjiDifficultyStorage.aStorage.concat(aIds));
     }
 
-    function getStorage(){
-        return oKanjiDifficultyStorage.aStorage;
-    }
-
+    /**
+     * Returns an array with as many kanji IDs as the required number. If there are not enough kanji IDs in the current
+     * cycle, more are extracted from the storage. If the number of kanji IDs required is bigger than the storage, the
+     * all stored kanji IDs are returned.
+     * @param nRequired The number of kanji IDs required
+     * @returns {Array} An array with the required kanji IDs.
+     */
     function getFromCycle(nRequired){
         var aCycleIds = [];
 
@@ -49,7 +74,7 @@ function KanjiDifficultyStorage(){
 
             oKanjiDifficultyStorage.aCurrentCycle = oKanjiDifficultyStorage.aStorage.slice();
 
-            var aPool = arrayDiff(oKanjiDifficultyStorage.aCurrentCycle, oKanjiDifficultyStorage.aStorage);
+            var aPool = arrayDiff(aCycleIds, oKanjiDifficultyStorage.aCurrentCycle);
 
             var sKanjiId;
             while(aCycleIds.length < nRequired){
@@ -63,16 +88,36 @@ function KanjiDifficultyStorage(){
         }
     }
 
+    /**
+     * Sets the current cycle array.
+     * @param aCycle The array to be set as the current cycle.
+     */
     function setCurrentCycle(aCycle){
         oKanjiDifficultyStorage.aCurrentCycle = aCycle;
     }
 
+    /**
+     * Returns the current cycle array.
+     * @returns {Array} The current cycle array.
+     */
     function getCurrentCycle(){
         return oKanjiDifficultyStorage.aCurrentCycle;
     }
 
+    /**
+     * Sets the storage array.
+     * @param aToStore The array to be set as the storage.
+     */
     function setStorage(aToStore){
         oKanjiDifficultyStorage.aStorage = aToStore;
+    }
+
+    /**
+     * Returns the storage array.
+     * @returns {Array} The storage array.
+     */
+    function getStorage(){
+        return oKanjiDifficultyStorage.aStorage;
     }
 
 }
