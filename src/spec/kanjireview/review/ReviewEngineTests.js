0d861d75c9f2a50a510a164bfb81e089d924800a
Carlos Llongo <carlos.llongo@gmail.com>
2015-10-19 19:54:11 +0200

    Added the additional values to the ReviewEngine constructor. Finished the tests for the 'can get next kanji' spec.

--- src/spec/kanjireview/review/ReviewEngineTests.js
+++ src/spec/kanjireview/review/ReviewEngineTests.js
@@ -1,39 +1,106 @@
 describe('ReviewEngine', function(){
 
     it('can create a ReviewEngine object', function(){
-        expect(new ReviewEngine()).toBeDefined();
+        var oReviewDataPersistenceMock = mockObject(new ReviewDataPersistence());
+        var oReviewDataMock = mockObject(new ReviewData(oReviewDataPersistenceMock));
+        var oReviewBatchMock = mockObject(new ReviewBatch());
+        var oKanjiCollectionMock = mockObject(new KanjiCollection());
+
+        expect(new ReviewEngine({
+            reviewData: oReviewDataMock,
+            kanjiCollection: oKanjiCollectionMock,
+            reviewBatch: oReviewBatchMock
+        })).toBeDefined();
+    });
+
+    it("can get learned kanji", function () {
+        var oReviewDataPersistenceMock = mockObject(new ReviewDataPersistence());
+        var oReviewDataMock = mockObject(new ReviewData(oReviewDataPersistenceMock));
+        var oReviewBatchMock = mockObject(new ReviewBatch());
+        var oKanjiCollectionMock = mockObject(new KanjiCollection());
+        createStub(oReviewDataMock, 'getLearnedKanji', 67);
+
+        var oReviewEngine = new ReviewEngine({
+            reviewData: oReviewDataMock,
+            kanjiCollection: oKanjiCollectionMock,
+            reviewBatch: oReviewBatchMock
+        });
+
+        expect(oReviewEngine.getLearnedKanji()).toBe(67);
     });
 
     it("can set a learned kanji number", function () {
-        var oKanjiCollection = new KanjiCollection();
+        var oReviewDataPersistenceMock = mockObject(new ReviewDataPersistence());
+        var oReviewDataMock = mockObject(new ReviewData(oReviewDataPersistenceMock));
+        var oReviewBatchMock = mockObject(new ReviewBatch());
+        var oKanjiCollectionMock = mockObject(new KanjiCollection());
+        spyFunction(oReviewDataMock, 'setLearnedKanji');
 
-        var oReviewEngine = new ReviewEngine(51, oKanjiCollection);
+        var oReviewEngine = new ReviewEngine({
+            reviewData: oReviewDataMock,
+            kanjiCollection: oKanjiCollectionMock,
+            reviewBatch: oReviewBatchMock
+        });
 
-        expect(oReviewEngine.getLearnedKanji()).toEqual(51);
+        oReviewEngine.setLearnedKanji(51);
+
+        expect(wasCalled(oReviewDataMock, 'setLearnedKanji')).toBe(true);
     });
 
     it("can get next kanji", function () {
-        var oKanjiCollection = new KanjiCollection();
+        var oReviewDataPersistenceMock = mockObject(new ReviewDataPersistence());
+        var oReviewDataMock = mockObject(new ReviewData(oReviewDataPersistenceMock));
+        createStub(oReviewDataMock, 'getLearnedKanji', 1);
 
-        oKanjiCollection.putKanji(new Kanji('1', '雨', 'rain'));
-        oKanjiCollection.putKanji(new Kanji('2', '雷', 'thunder'));
-        oKanjiCollection.putKanji(new Kanji('3', '雪', 'snow'));
-        oKanjiCollection.putKanji(new Kanji('4', '水', 'water'));
+        var oKanjiMock = mockObject(new Kanji());
+        createStub(oKanjiMock, 'getId', '1');
+        createStub(oKanjiMock, 'getCharacter', '水');
+        createStub(oKanjiMock, 'getKeyword', 'water');
+        var oKanjiCollectionMock = mockObject(new KanjiCollection());
+        createStub(oKanjiCollectionMock, 'getKanji', oKanjiMock);
 
-        var oReviewEngine = new ReviewEngine(oKanjiCollection);
+        var oReviewBatchMock = mockObject(new ReviewBatch());
+        createStub(oReviewBatchMock, 'getBatch', [1]);
+
+        var oReviewEngine = new ReviewEngine({
+            reviewData: oReviewDataMock,
+            kanjiCollection: oKanjiCollectionMock,
+            reviewBatch: oReviewBatchMock
+        });
 
         var oNextKanji = oReviewEngine.getNextKanji();
 
-        expect(typeof oNextKanji).toEqual('object');
-        expect(typeof oNextKanji.getId()).toEqual('string');
-        expect(typeof oNextKanji.getCharacter()).toEqual('string');
-        expect(typeof oNextKanji.getKeyword()).toEqual('string');
+        expect(oNextKanji.getId()).toEqual('1');
 
-        var oAnotherKanji = oReviewEngine.getNextKanji();
 
-        //expect(oAnotherKanji).not.toEqual(oNextKanji);
+        // Get consecutive next kanjis
+        var aKanjiMocks = [mockObject(new Kanji()), mockObject(new Kanji())];
+        createStub(aKanjiMocks[0], 'getId', '1');
+        createStub(aKanjiMocks[0], 'getCharacter', '水');
+        createStub(aKanjiMocks[0], 'getKeyword', 'water');
+        createStub(aKanjiMocks[1], 'getId', '2');
+        createStub(aKanjiMocks[1], 'getCharacter', '雷');
+        createStub(aKanjiMocks[1], 'getKeyword', 'thunder');
+        createStub(oKanjiCollectionMock, 'getKanji', aKanjiMocks, true);
+        createStub(oReviewDataMock, 'getLearnedKanji', 2);
+        createStub(oReviewBatchMock, 'getBatch', [1, 2]);
+
+        oReviewEngine = new ReviewEngine({
+            reviewData: oReviewDataMock,
+            kanjiCollection: oKanjiCollectionMock,
+            reviewBatch: oReviewBatchMock
+        });
+
+        oNextKanji = oReviewEngine.getNextKanji();
+        var sKanjiId = oNextKanji.getId();
+        expect(sKanjiId).toEqual('1');
+
+        oNextKanji = oReviewEngine.getNextKanji();
+        var sNextKanjiId = oNextKanji.getId();
+        expect(sNextKanjiId).toEqual('2');
+
+        oNextKanji = oReviewEngine.getNextKanji();
+        var sAnotherKanjiId = oNextKanji.getId();
+        expect(['1','2'].indexOf(sAnotherKanjiId)).not.toEqual(-1);
     });
-
-
-
 });
