25f37f6adf6c5ffc61892dd9eb624d123224a2db
Carlos Llongo <carlos.llongo@gmail.com>
2015-10-26 14:15:19 +0100

    Update the test to the new way the data is obtained from the persistence object.

--- src/spec/kanjireview/review/ReviewDataTests.js
+++ src/spec/kanjireview/review/ReviewDataTests.js
@@ -1,34 +1,40 @@
 describe('ReviewData', function(){
 
     it("can init storages", function () {
-        var oReviewDataPersistenceMock = mockObject(new ReviewDataPersistence());
-        createStub(oReviewDataPersistenceMock, 'getHardStorage', [11, 12, 13, 14, 15]);
-        createStub(oReviewDataPersistenceMock, 'getMediumStorage', [6, 7, 8, 9, 10]);
-        createStub(oReviewDataPersistenceMock, 'getEasyStorage', [1, 2, 3, 4, 5]);
+        var oReviewDataPersistenceMock = mockObject({getReviewData: function (){}});
+        createStub(oReviewDataPersistenceMock, 'getReviewData', {
+            hardStorage: [11, 12, 13, 14, 15],
+            mediumStorage: [6, 7, 8, 9, 10],
+            easyStorage: [1, 2, 3, 4, 5]
+        });
 
-        var oReviewData = new ReviewData(oReviewDataPersistenceMock);
+        var oReviewData = new ReviewData({reviewDataPersistence: oReviewDataPersistenceMock});
         expect(oReviewData.getHardStorage()).toEqual([11, 12, 13, 14, 15]);
         expect(oReviewData.getMediumStorage()).toEqual([6, 7, 8, 9, 10]);
         expect(oReviewData.getEasyStorage()).toEqual([1, 2, 3, 4, 5]);
     });
 
     it("can init cycles", function () {
-        var oReviewDataPersistenceMock = mockObject(new ReviewDataPersistence());
-        createStub(oReviewDataPersistenceMock, 'getHardCycle', [11, 12, 13, 14, 15]);
-        createStub(oReviewDataPersistenceMock, 'getMediumCycle', [6, 7, 8, 9, 10]);
-        createStub(oReviewDataPersistenceMock, 'getEasyCycle', [1, 2, 3, 4, 5]);
+        var oReviewDataPersistenceMock = mockObject({getReviewData: function (){}});
+        createStub(oReviewDataPersistenceMock, 'getReviewData', {
+            hardCycle: [11, 12, 13, 14, 15],
+            mediumCycle: [6, 7, 8, 9, 10],
+            easyCycle: [1, 2, 3, 4, 5]
+        });
 
-        var oReviewData = new ReviewData(oReviewDataPersistenceMock);
+        var oReviewData = new ReviewData({reviewDataPersistence: oReviewDataPersistenceMock});
         expect(oReviewData.getHardCycle()).toEqual([11, 12, 13, 14, 15]);
         expect(oReviewData.getMediumCycle()).toEqual([6, 7, 8, 9, 10]);
         expect(oReviewData.getEasyCycle()).toEqual([1, 2, 3, 4, 5]);
     });
 
     it("can init learned kanji", function () {
-        var oReviewDataPersistenceMock = mockObject(new ReviewDataPersistence());
-        createStub(oReviewDataPersistenceMock, 'getLearnedKanji', 1342);
+        var oReviewDataPersistenceMock = mockObject({getReviewData: function (){}});
+        createStub(oReviewDataPersistenceMock, 'getReviewData', {
+            learnedKanji: 1342
+        });
 
-        var oReviewData = new ReviewData(oReviewDataPersistenceMock);
+        var oReviewData = new ReviewData({reviewDataPersistence: oReviewDataPersistenceMock});
         expect(oReviewData.getLearnedKanji()).toEqual(1342);
     });
 });
