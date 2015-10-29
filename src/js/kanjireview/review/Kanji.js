b85ca4346ee2707bf4148e8d803e6281d929aae2
Carlos Llongo <carlos.llongo@gmail.com>
2015-10-29 16:43:33 +0100

    Added documentation.

--- src/js/kanjireview/review/Kanji.js
+++ src/js/kanjireview/review/Kanji.js
@@ -1,19 +1,36 @@
-'use strict';
-
+/**
+ * This class represents a Kanji, with ist ID, its japanese character and the Heisig keyword.
+ * @param sId The Kanji ID
+ * @param sCharacter The Kanji japanese character
+ * @param sKeyword The keyword
+ * @constructor
+ */
 function Kanji(sId, sCharacter, sKeyword){
     this.sId = sId;
     this.sCharacter = sCharacter;
     this.sKeyword = sKeyword;
 }
 
+/**
+ * Obtains the ID of the kanji
+ * @returns {String|Kanji.sId} The ID of the kanji
+ */
 Kanji.prototype.getId = function(){
     return this.sId;
 }
 
+/**
+ * Obtains the japanese character
+ * @returns {String|Kanji.sCharacter} The japanese character
+ */
 Kanji.prototype.getCharacter = function(){
     return this.sCharacter;
 }
 
+/**
+ * Obtains the Heisig keyword
+ * @returns {String|Kanji.sKeyword} The keyword used to learn the kanji
+ */
 Kanji.prototype.getKeyword = function(){
     return this.sKeyword;
 }
